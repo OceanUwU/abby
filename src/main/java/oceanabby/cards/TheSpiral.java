3bb832e302c82fbd0a30d0c72e840b83b97c6421
ocean <ocean@ocean.lol>
2025-01-18 10:32:33 +0000

    spiral has its evo

--- src/main/java/oceanabby/cards/TheSpiral.java
+++ src/main/java/oceanabby/cards/TheSpiral.java
@@ -2,8 +2,10 @@ package oceanabby.cards;
 
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import com.megacrit.cardcrawl.powers.PlatedArmorPower;
 
 import static oceanabby.AbbyMod.makeID;
+import static oceanabby.util.Wiz.applyToSelf;
 
 public class TheSpiral extends AbstractAbbyCard {
     public final static String ID = makeID("TheSpiral");
@@ -11,10 +13,13 @@ public class TheSpiral extends AbstractAbbyCard {
     public TheSpiral() {
         super(ID, 2, CardType.SKILL, CardRarity.UNCOMMON, CardTarget.SELF);
         setMagic(2, +1);
+        setSecondMagic(1);
         cardsToPreview = new HardShell();
     }
 
     public void use(AbstractPlayer p, AbstractMonster m) {
+        if (evod)
+            applyToSelf(new PlatedArmorPower(p, secondMagic));
         addAptation();
     }
 }
